92a397f305dca4b0760c12a2ec346d2f96bd8b51
Oliver Dumhart <oliver@dumhart.net>
2020-12-30 11:47:04 +0100

    Update stat service

--- stat-service/src/main/java/at/technikumwien/stat/StatResource.java
+++ stat-service/src/main/java/at/technikumwien/stat/StatResource.java
@@ -2,22 +2,22 @@ package at.technikumwien.stat;
 
 import at.technikumwien.stat.entities.LandmarkStat;
 import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/resources/stat")
-@Log
+@Slf4j
 public class StatResource
 {
     @Autowired
     private LandmarkStatRepository landmarkStatRepository;
 
-    @GetMapping
+    @GetMapping("landmarks")
+    //@CrossOrigin("*")
     public List<LandmarkStat> hello() {
-        log.info("wow! It works!");
         log.info("wow! It works!");
         return landmarkStatRepository.findAll();
     }
